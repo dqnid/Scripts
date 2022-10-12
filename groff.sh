c53ba054b985d3ed2038691f4d9d2fbf6beb8616
dqnid <dani.heras@hotmail.com>
2022-10-12 12:20:51 +0200

    New local folder structure

--- groff.sh
+++ groff.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-groff -ms -D UTF-8 ~/Documentos/Anotaciones/Ficheros\ Fuente/Documentación_Linux.ms -T pdf > ~/Documentos/Anotaciones/Documentación_Linux.pdf
-groff -ms -D UTF-8 ~/Documentos/Anotaciones/Ficheros\ Fuente/Atajos.ms -T pdf > ~/Documentos/Anotaciones/Atajos.pdf
+groff -ms -D UTF-8 ~/Documents/Anotaciones/Ficheros\ Fuente/Documentación_Linux.ms -T pdf > ~/Documents/Anotaciones/Documentación_Linux.pdf
+groff -ms -D UTF-8 ~/Documents/Anotaciones/Ficheros\ Fuente/Atajos.ms -T pdf > ~/Documents/Anotaciones/Atajos.pdf
 if test $1 -eq 1; then
-	mupdf ~/Documentos/Anotaciones/Documentación_Linux.pdf
+	mupdf ~/Documents/Anotaciones/Documentación_Linux.pdf
 else
-	mupdf ~/Documentos/Anotaciones/Atajos.pdf
+	mupdf ~/Documents/Anotaciones/Atajos.pdf
 fi
--- sincV5.sh
+++ sincV5.sh
@@ -5,7 +5,7 @@ ubicaciones=("Anotaciones" "Proyectos" "Universidad")
 
 for i in "${ubicaciones[@]}"
 do
-	rsync -vrP -e "ssh -i ~/.ssh/id_rsa_dqnid" --delete-after /home/danih/Documentos/$i dqnid@dqnid.com:/home/dqnid/Documentos
+	rsync -vrP -e "ssh -i ~/.ssh/id_rsa_dqnid" --delete-after /home/danih/Documents/$i dqnid@dqnid.com:/home/dqnid/Documents
 	if test $? -eq 0; then
 		notify-send "Éxito - $i" "$i sincronizadas correctamente"
 	else
