d9753e7b49c4d9c759520df50d262fd0a2568e15
dqnid <dani.heras@hotmail.com>
2022-10-14 01:52:12 +0200

    Using bin folder if exists

--- compilaKotlin.sh
+++ compilaKotlin.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
+bin="bin"
 if test $# -eq 1; then
 	dir=$(pwd)
 	jar=$(echo $1 | awk -F'.kt' '{print $1}')
-	kotlinc "$dir/$1" -include-runtime -d "$dir/$jar.jar"
+	if test -d "$dir/$bin"; then
+		kotlinc "$dir/$1" -include-runtime -d "$dir/$bin/$jar.jar"
+	else
+		kotlinc "$dir/$1" -include-runtime -d "$dir/$jar.jar"
+	fi
 elif test $# -eq 2; then
 	dir=$(pwd)
 	jar=$(echo $2 | awk -F'.kt' '{print $1}')
 	if test $1 -eq 1; then
-		java -jar "$dir/$jar.jar"
+		if test -d "$dir/$bin"; then
+			kotlinc "$dir/$2" -include-runtime -d "$dir/$bin/$jar.jar"
+			java -jar "$dir/$bin/$jar.jar"
+		else 
+			kotlinc "$dir/$2" -include-runtime -d "$dir/$jar.jar"
+			java -jar "$dir/$jar.jar"
+		fi
 	fi
 else
-	echo "Modo de uso:"
-	echo -e " $ \u001b[32mck nombre.kt \u001b[0m"
-	echo -e " $ \u001b[33mck nombre.kt 1\u001b[0m"
+	echo "Use:"
+	echo -e " $ \u001b[32mck name.kt \u001b[0m"
+	echo -e " $ \u001b[33mck name.kt 1\u001b[0m"
+	echo -e "**note: if a bin folder exists, will output .jar there"
 	exit 1
 fi
