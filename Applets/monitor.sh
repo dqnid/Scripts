dea3a59aa2d703e653ee08644c6c285332833deb
dqnid <dani.heras@hotmail.com>
2023-05-27 15:41:59 +0200

    Transparency changed

--- Applets/monitor.sh
+++ Applets/monitor.sh
@@ -22,13 +22,16 @@ if xrandr | grep "$externo connected" && xrandr | grep "$externo2 connected"; th
 	;;
 	esac
 elif xrandr | grep "$externo connected"; then
-	eleccion="$(rofi -no-config -no-lazy-grab -sep "|" -dmenu -i -p 'System' -width 12 -line-padding 3 -lines 6 -theme /home/danih/Scripts/rofi/monitor.rasi <<< "Dual Side|Dual Top|Dual Side Vertical|Interna|Externa|Duplicar")"
+	eleccion="$(rofi -no-config -no-lazy-grab -sep "|" -dmenu -i -p 'System' -width 12 -line-padding 3 -lines 6 -theme /home/danih/Scripts/rofi/monitor.rasi <<< "Dual Side|Dual Top|Dual Side Vertical|Dual Side Low|Interna|Externa|Duplicar")"
 	case "$eleccion" in
 	"Dual Top")
 		xrandr --output "$interno" --auto --mode 1920x1080 --output "$externo" --auto --above "$interno" --output "$externo2" --off
 	;;
 	"Dual Side")
 		xrandr --output HDMI-A-0 --primary --mode 1920x1080 --rate 120 --pos 1920x0 --rotate normal --output eDP --mode 1920x1080 --rate 60 --pos 0x600 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off
+	;;
+	"Dual Side Low")
+		xrandr --output HDMI-A-0 --mode 1600x900 --pos 1920x0 --rotate normal --output eDP --primary --mode 1920x1080 --rate 60 --pos 0x315 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off
 	;;
 	"Dual Side Vertical")
 	xrandr --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate right --output eDP --primary --mode 1920x1080 --pos 1080x1762 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off	
