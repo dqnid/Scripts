a9bd92107264fe242e5165beb69b0dc0b268a335
dqnid <dani.heras@hotmail.com>
2023-05-28 00:20:43 +0200

    Styles parameterized

--- System/confirmKilli3.sh
+++ System/confirmKilli3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-confirm="$(rofi -no-config -no-lazy-grab -sep "|" -dmenu -i -p '¿Cerrar ventana?' -width 12 -line-padding 3 -lines 3 -theme /home/danih/Scripts/Temas/confirm_kill.rasi <<< "No|Si")"
+confirm="$(rofi -no-config -no-lazy-grab -sep "|" -dmenu -i -p '¿Cerrar ventana?' -width 12 -line-padding 3 -lines 3 -theme /home/danih/Scripts/rofi/confirm_kill.rasi <<< "No|Si")"
 case "$confirm" in
 	Si)
 		echo "Si confirmado"
