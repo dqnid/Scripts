75ff8ce94255f6d9e4341eecf8f77a261ee67a1a
dqnid <dani.heras@hotmail.com>
2023-06-15 22:37:07 +0200

    style to dark grey + new screen

--- Applets/monitor.sh
+++ Applets/monitor.sh
@@ -28,7 +28,7 @@ elif xrandr | grep "$externo connected"; then
 		xrandr --output "$interno" --auto --mode 1920x1080 --output "$externo" --auto --above "$interno" --output "$externo2" --off
 	;;
 	"Dual Side")
-		xrandr --output HDMI-A-0 --primary --mode 1920x1080 --rate 120 --pos 1920x0 --rotate normal --output eDP --mode 1920x1080 --rate 60 --pos 0x600 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off
+		xrandr --output HDMI-A-0 --primary --mode 1920x1080 --rate 60 --pos 1920x0 --rotate normal --output eDP --mode 1920x1080 --rate 60 --pos 0x600 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off
 	;;
 	"Dual Side Low")
 		xrandr --output HDMI-A-0 --mode 1600x900 --pos 1920x0 --rotate normal --output eDP --primary --mode 1920x1080 --rate 60 --pos 0x315 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off
